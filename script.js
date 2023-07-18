3658e5c2fa47d521af4438d2c9f4ffbc46b094eb
Mohit kachhaway <130671896+MohitKachhway@users.noreply.github.com>
2023-07-18 19:17:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function threeSum(arr, target) {
-// write your code here
-  
+  let n=arr.length;
+	let arr2=[];
+	let ind=0;
+  for(let i=0;i<n-2;++i){
+	  for(let j=i+1;j<n-1;++j){
+		  for(let k=j+1;k<n;++k){
+			  arr2[ind]=arr[i]+arr[j]+arr[k];
+        ++ind;
+		  }
+	  }
+  }
+	
+  let closest=arr2[0];
+  for(let i=0;i<arr2.length;++i){
+     let curr=arr2[i];
+     let currDiff=Math.abs(curr-target)
+     let closeDiff=Math.abs(closest-target)
+	  
+	  if(currDiff<closeDiff){
+	    closest=curr;
+	  }
+    }
+	return closest;
 }
+// let arr=[-1 , 2 , 1, -4];
+// let target=1;
+// let arr2=threeSum(arr, target) 
+
+// console.log(closest);
+// module.exports = threeSum;
 
 module.exports = threeSum;
